dcc68f8f4e5556370a2ccec481bc949b62fdfe05
Sarah Marmorosch <sarah.marmorosch@swisstph.ch>
2026-05-07 15:45:21 +0200

    update directories

--- analysis_pipeline/read_processing/005_gstacks_albo.sh
+++ analysis_pipeline/read_processing/005_gstacks_albo.sh
@@ -13,7 +13,7 @@
 GSTACKS=/cvmfs/soft.computecanada.ca/easybuild/software/2023/x86-64-v3/Compiler/gcccore/stacks/2.67/bin/gstacks
 
 $GSTACKS \
--I /scicore/home/muellepi/GROUP/albopictus/alignments/AlboAlign/NewGenome/ \
+-I /scicore/home/muellepi/marmor0000/albopictus_ddRADseq/alignment/ \
 -S _aligned_sorted_filtered.bam \
 -M /scicore/home/muellepi/marmor0000/albopictus_ddRADseq/snp_calling/popmap_albo.txt \
 -O /scicore/home/muellepi/marmor0000/albopictus_ddRADseq/snp_calling/gstacks/ \
