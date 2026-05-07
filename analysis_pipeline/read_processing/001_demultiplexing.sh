8b04a9e55401afe74207d4f29bcadfb6b5fdf5ec
Sarah Marmorosch <sarah.marmorosch@swisstph.ch>
2026-05-07 11:12:18 +0200

    demultiplex reads of library 1

--- analysis_pipeline/read_processing/001_demultiplexing.sh
+++ analysis_pipeline/read_processing/001_demultiplexing.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 #SBATCH --job-name=demux_albo
-#SBATCH --mem=20G
-#SBATCH --cpus-per-task=6
-#SBATCH --time=7-00:00:00
-#SBATCH --qos=1week
-#SBATCH --array=1-5%5
-#SBATCH --output=/scicore/home/muellepi/marmor0000/albopictus_ddRADseq/cleaned/err_out/demux_%a.out
-#SBATCH --error=/scicore/home/muellepi/marmor0000/albopictus_ddRADseq/cleaned/err_out/demux_%a.err
+#SBATCH --mem=16G
+#SBATCH --cpus-per-task=4
+#SBATCH --time=24:00:00
+#SBATCH --qos=24hours
+#SBATCH --output=/scicore/home/muellepi/marmor0000/albopictus_ddRADseq/demultiplexed/err_out/logs.out
+#SBATCH --error=/scicore/home/muellepi/marmor0000/albopictus_ddRADseq/demultiplexed/err_out/log.err
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=sarah.marmorosch@swisstph.ch
 
@@ -15,25 +14,18 @@ set -euo pipefail
 module load Stacks/2.2-foss-2016b
 
 # Paths
-RAW_DIR=/scicore/home/muellepi/marmor0000/albopictus_ddRADseq/raw_data
-BARCODE_DIR=${RAW_DIR}/barcodes
-OUT_BASE=/scicore/home/muellepi/marmor0000/albopictus_ddRADseq/cleaned
-
-# Map SLURM array index → library name
-LIBS=(Final_albo_1 Final_albo_2 Final_albo_3 Final_albo_4 Final_albo_5)
-
-LIB=${LIBS[$SLURM_ARRAY_TASK_ID-1]}
-
-OUT_DIR=${OUT_BASE}
+RAW_DIR=/scicore/home/muellepi/marmor0000/albopictus_ddRADseq/raw_data/
+BARCODE_DIR=/scicore/home/muellepi/marmor0000/albopictus_ddRADseq/raw_data/barcodes/albo_1.txt
+OUT_DIR=/scicore/home/muellepi/marmor0000/albopictus_ddRADseq/demultiplexed
 
 process_radtags \
     -P \
     -p $RAW_DIR \
-    -b ${BARCODE_DIR}/albo_${SLURM_ARRAY_TASK_ID}.txt \
+    -b ${BARCODE_DIR} \
     -o $OUT_DIR \
     -c -q -r \
     --inline_inline \
     --renz_1 nlaIII \
     --renz_2 mluCI \
-    --filename-filter $LIB
+    --filename-filter Final_albo_1
 
