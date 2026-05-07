f8887ebb07f0c5471d6597ebbc2e5c54cf110b61
Sarah Marmorosch <sarah.marmorosch@swisstph.ch>
2026-05-07 13:05:19 +0200

    changed directories

--- analysis_pipeline/read_processing/003_multiqc_demu.sh
+++ analysis_pipeline/read_processing/003_multiqc_demu.sh
@@ -4,13 +4,13 @@
 #SBATCH --mem-per-cpu=4G
 #SBATCH --time=01:00:00
 #SBATCH --qos=6hours
-#SBATCH --output=/scicore/home/muellepi/marmor0000/albopictus_ddRADseq/fastqc/multiqc.log
+#SBATCH --output=/scicore/home/muellepi/marmor0000/albopictus_ddRADseq/fastqc/fastqc_multiqc_demu/multiqc.log
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=sarah.marmorosch@swisstph.ch
 
 module load MultiQC
 
-INPUT=/scicore/home/muellepi/marmor0000/albopictus_ddRADseq/fastqc/
-OUTPUT=/scicore/home/muellepi/marmor0000/albopictus_ddRADseq/fastqc/
+INPUT=/scicore/home/muellepi/marmor0000/albopictus_ddRADseq/fastqc/fastqc_multiqc_demu/
+OUTPUT=/scicore/home/muellepi/marmor0000/albopictus_ddRADseq/fastqc/fastqc_multiqc_demu/
 
 multiqc $INPUT -o $OUTPUT
