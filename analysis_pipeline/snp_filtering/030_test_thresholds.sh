e06491c40754cd9625e06529bf43bf086cf1c304
Sarah Marmorosch <sarah.marmorosch@swisstph.ch>
2026-07-31 14:48:21 +0200

    change directory

--- analysis_pipeline/snp_filtering/030_test_thresholds.sh
+++ analysis_pipeline/snp_filtering/030_test_thresholds.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-#SBATCH --job-name=031_test_thresholds
+#SBATCH --job-name=030_test_thresholds
 #SBATCH --mem=1G
 #SBATCH --cpus-per-task=1
 #SBATCH --time=00:30:00
 #SBATCH --qos=30min
-#SBATCH --output=/scicore/home/muellepi/marmor0000/albopictus_ddRADseq/snp_filtering/031_test_thresholds/err_out/031_test_thresholds.%a.out
-#SBATCH --error=/scicore/home/muellepi/marmor0000/albopictus_ddRADseq/snp_filtering/031_test_thresholds/err_out/031_test_thresholds.%a.err
+#SBATCH --output=/scicore/home/muellepi/marmor0000/albopictus_ddRADseq/snp_filtering/031_test_thresholds/err_out/030_test_thresholds.%a.out
+#SBATCH --error=/scicore/home/muellepi/marmor0000/albopictus_ddRADseq/snp_filtering/031_test_thresholds/err_out/030_test_thresholds.%a.err
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=sarah.marmorosch@swisstph.ch
 #SBATCH --array=1-56%56
 
 ml VCFtools
 
-VCF_IN=/scicore/home/muellepi/marmor0000/albopictus_ddRADseq/snp_filtering/021_inspect_metrics/populations.snps.filtered2.vcf.gz
+VCF_IN=/scicore/home/muellepi/marmor0000/albopictus_ddRADseq/snp_filtering/020_preliminary_filters/populations.snps.filtered2.vcf.gz
 SCRIPT_DIR=/scicore/home/muellepi/marmor0000/git_repositories/albopictus_pop_gen/analysis_pipeline/utils/
 
-SEEDFILE=/scicore/home/muellepi/marmor0000/git_repositories/albopictus_pop_gen/analysis_pipeline/snp_filtering//filter_parameters.csv
+SEEDFILE=/scicore/home/muellepi/marmor0000/git_repositories/albopictus_pop_gen/analysis_pipeline/snp_filtering/030_filter_parameters.csv
 SEED_STR=$(sed -n ${SLURM_ARRAY_TASK_ID}p $SEEDFILE)
 IFS=',' read -r -a SEED_ARRAY <<< "$SEED_STR"
 
@@ -26,7 +26,7 @@ MAX_DEPTH=30
 
 EXP=filter_FM_${MISS}_mD_${MIN_DEPTH}_MD_${MAX_DEPTH}
 
-OUT_FOLDER=/scicore/home/muellepi/marmor0000/albopictus_ddRADseq/snp_filtering/031_test_thresholds/031_$EXP
+OUT_FOLDER=/scicore/home/muellepi/marmor0000/albopictus_ddRADseq/snp_filtering/030_test_thresholds/031_$EXP
 mkdir -p $OUT_FOLDER
 
 
